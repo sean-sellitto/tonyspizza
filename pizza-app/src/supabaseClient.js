d515e033c384c557322f766f2bfa3e9ab6df4f66
sean-sellitto <sean.sellitto@gmail.com>
2026-01-06 08:21:01 -0500

    moved env vars

--- pizza-app/src/supabaseClient.js
+++ pizza-app/src/supabaseClient.js
@@ -1,8 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ajqeivmkvfgejmmhygdg.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqcWVpdm1rdmZnZWptbWh5Z2RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MzgzNjUsImV4cCI6MjA4MTQxNDM2NX0.ar2DQYrykVJhnVEF5wMu6_S8pOLhEmZizf1pZg2Q0Qg";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
